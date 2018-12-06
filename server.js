4b3b6a4c37554c772a23feda9af18f7b97a8306e
matt fyke <mfyke@live.com>
2018-12-05 23:15:42 -0800

    files created

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+
+app.use(express.static("public"));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(methodOverride("_method"));
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/index.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
